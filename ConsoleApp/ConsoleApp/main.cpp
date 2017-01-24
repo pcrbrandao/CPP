840c6c967514ab7d35f2b36b3f6ece47dd9ff1a3
Pedro Brandao <pcrbrandao@gmail.com>
2017-01-24 16:52:27 -0200

    Relembrando C++

---
+++ ConsoleApp/ConsoleApp/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  ConsoleApp
+//
+//  Created by Pedro Brandão on 24/01/17.
+//  Copyright © 2017 Pedro Brandão. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    printf("Ou assim, com o printf\n\n");
+    return 0;
+}
